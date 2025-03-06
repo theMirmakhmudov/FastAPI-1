cea5aaf1b11effaebdaadddf9b570193f6e226f9
theMirmakhmudov <mr.mirmakhmudov16112008@gmail.com>
2025-03-06 08:36:52 +0500

    Commit

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-from .model import Users
+from models import Users
 
 app = FastAPI()
 
+video_db = {}
 
 @app.get('/')
 async def root():
@@ -25,5 +26,33 @@ async def get_users():
 
 
 @app.get("/models/{model_name}")
-async def get_model(user: Users):
-    return {"user": user}
+async def get_model(model_name: Users):
+    return {"model_name": model_name}
+
+
+@app.post("/api/user/{video_id}/like")
+async def like_video(video_id: int):
+    if video_id not in video_db:
+        video_db[video_id] = {"likes": 0, "views": 0}
+
+    video_db[video_id]["likes"] += 1
+
+    return {
+        "message": "Video yoqdi",
+        "video_id": video_id,
+        "total_likes": video_db[video_id]["likes"]
+    }
+
+
+@app.post("/api/user/{video_id}/viewer")
+async def view_video(video_id: int):
+    if video_id not in video_db:
+        video_db[video_id] = {"likes": 0, "views": 0}
+
+    video_db[video_id]["views"] += 1
+
+    return {
+        "message": "Video ko'rildi",
+        "video_id": video_id,
+        "total_views": video_db[video_id]["views"]
+    }
