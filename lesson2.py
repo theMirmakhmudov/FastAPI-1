8803df179fddd38c8bbdc2da537bae2f175acdf5
theMirmakhmudov <mr.mirmakhmudov16112008@gmail.com>
2025-03-12 12:05:35 +0500

    Commit

--- lesson2.py
+++ lesson2.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from fastapi import FastAPI, Query
 from models2 import Item
 
 app = FastAPI()
@@ -14,3 +16,17 @@ async def update_item(item_id: int, item: Item, q: str | None = None):
     if q:
         result.update({"q": q})
     return result
+
+@app.get("/items/")
+async def read_item(q: Annotated[str | None, Query(max_length=3)]):
+    results = {"item": [{"item": "foo"}, {"item": "bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+@app.get("/items/array/")
+async def read_item(q: Annotated[list[str] | None, Query()] = None):
+    query_items = {
+        "q": q
+    }
+    return query_items
